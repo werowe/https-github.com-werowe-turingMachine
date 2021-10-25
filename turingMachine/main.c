4f06e45f44076f9f828200442b4d80654f8c4d1d
werowe <werowe@walkerrowe.com>
2021-10-25 15:16:23 +0300

    xxxxxx

--- turingMachine/main.c
+++ turingMachine/main.c
@@ -24,33 +24,33 @@ int main(int argc, const char * argv[]) {
     struct Configuration configs[500];
     struct Configuration *ptrConfig = &configs[0];
     
-    char tape[500] = " " ;
+    char tape[] = "                                                                                                                  " ;
     char *ptrTape=tape;
     
     int i = 0;
     
     struct Configuration config = {
-      "b",
+      atoi("b"),
         "None",
         {"P0","R"},
-        "c"
+        atoi("c")
     };
   
 
     configs[i++]=config;
     
-    strcpy(config.mConfig,"c");
+    strcpy(config.mConfig,atoi("c"));
     strcpy(config.symbol,"None");
     strcpy(config.operations[0],"R");
-    strcpy(config.finalMConfig,"e");
+    strcpy(config.finalMConfig,atoi("e"));
       
     configs[i++]=config;
     
-    strcpy(config.mConfig,"e");
+    strcpy(config.mConfig,atoi("e"));
     strcpy(config.symbol,"None");
     strcpy(config.operations[0],"P1");
     strcpy(config.operations[1],"R");
-    strcpy(config.finalMConfig,"f");
+    strcpy(config.finalMConfig,atoi("f"));
       
     configs[i++]=config;
     
@@ -62,33 +62,46 @@ int main(int argc, const char * argv[]) {
     configs[i++]=config;
     
    
-    int k = 0;
+    int times = 0;
+    int stop = 20;
+    int idx=0;
     
+    int whichConfig=0;
     
-    while (k<i) {
-        config = configs[k];
+    while (times<stop) {
+        if (times==0) {
+            config = configs[whichConfig];
+        }
         
-        printf("%s\n",config.mConfig);
-        
-
-      
-                
+        printf("mConfig %s finalMConfig %s \n",config.mConfig, config.finalMConfig);
+    
         if (strcmp(config.symbol,"None") == 0 ) {
                 
-                if ( strcmp(ptrTape," ") == 0) {
-                    
+                if ( strcmp(tape[idx]," ") == 0) {
                     printf("is empty\n");
     
                 for ( int j = 0; j < 3;j++) {
                     printf("%s\n",config.operations[j]);
+                    
+                    if ( strcmp(config.operations[j],"P0") == 0) {
+                        strcpy(tape[idx],"0");
+                       }
+                    if ( strcmp(config.operations[j],"P1") == 0) {
+                        strcpy(tape[idx],"1");
+                       }
+                    if ( strcmp(config.operations[j],"R") == 0) {
+                        idx++;
+                       }
+                    printf("ptrTape in loop %s",ptrTape );
                     }
                 }
         }
         
-                  ptrTape++;
-        k++;
+        config = configs[atoi(config.finalMConfig)];
+        times++;
     }
     
+    printf("ptrTape %s",ptrTape );
     printf("done\n");
     
     
