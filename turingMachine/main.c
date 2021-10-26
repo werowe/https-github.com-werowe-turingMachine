58b3012192371832fa74be9db903343917b2ffea
werowe <werowe@walkerrowe.com>
2021-10-26 09:53:30 +0300

    working 0 1 0 1 0 1

--- turingMachine/main.c
+++ turingMachine/main.c
@@ -9,14 +9,11 @@
 #include <strings.h>
 #include <stdlib.h>
 
-
- 
-
 struct Configuration {
-   char  mConfig[2];
+   int mConfig;
    char  symbol[50];
    char  operations[5][3];
-   char  finalMConfig[2];
+   int finalMConfig;
 };
 
 int main(int argc, const char * argv[]) {
@@ -30,41 +27,41 @@ int main(int argc, const char * argv[]) {
     int i = 0;
     
     struct Configuration config = {
-      atoi("b"),
+      (int) 'b',
         "None",
         {"P0","R"},
-        atoi("c")
+        (int) 'c'
     };
   
 
     configs[i++]=config;
     
-    strcpy(config.mConfig,atoi("c"));
+    config.mConfig=(int) 'c';
     strcpy(config.symbol,"None");
     strcpy(config.operations[0],"R");
-    strcpy(config.finalMConfig,atoi("e"));
+    config.finalMConfig=(int)('e');
       
     configs[i++]=config;
     
-    strcpy(config.mConfig,atoi("e"));
+    config.mConfig=(int) 'e';
     strcpy(config.symbol,"None");
     strcpy(config.operations[0],"P1");
     strcpy(config.operations[1],"R");
-    strcpy(config.finalMConfig,atoi("f"));
+    config.finalMConfig=(int) 'f';
       
     configs[i++]=config;
     
-    strcpy(config.mConfig,"f");
+    config.mConfig = (int) 'f';
     strcpy(config.symbol,"None");
     strcpy(config.operations[0],"R");
-    strcpy(config.finalMConfig,"b");
+    config.finalMConfig = (int) 'b';
       
     configs[i++]=config;
     
    
-    int times = 0;
-    int stop = 20;
-    int idx=0;
+    int times = 0; // control number of runs
+    int stop = 20; // control number of runs
+  
     
     int whichConfig=0;
     
@@ -73,35 +70,51 @@ int main(int argc, const char * argv[]) {
             config = configs[whichConfig];
         }
         
-        printf("mConfig %s finalMConfig %s \n",config.mConfig, config.finalMConfig);
+        printf("mConfig %c finalMConfig %c \n",(char) config.mConfig, (char) config.finalMConfig);
     
+        /* if tape position is None */
         if (strcmp(config.symbol,"None") == 0 ) {
                 
-                if ( strcmp(tape[idx]," ") == 0) {
-                    printf("is empty\n");
+        
+            // loop through array of operations
     
                 for ( int j = 0; j < 3;j++) {
-                    printf("%s\n",config.operations[j]);
+                  
                     
                     if ( strcmp(config.operations[j],"P0") == 0) {
-                        strcpy(tape[idx],"0");
+                        *ptrTape='0';
                        }
                     if ( strcmp(config.operations[j],"P1") == 0) {
-                        strcpy(tape[idx],"1");
+                        *ptrTape='1';
                        }
                     if ( strcmp(config.operations[j],"R") == 0) {
-                        idx++;
+                        ptrTape++;
                        }
-                    printf("ptrTape in loop %s",ptrTape );
+                    
+                    
+                    // loop through and find the config to load.  it should be finalMConfig
+                
+                        
                     }
+            
+            printf("search for %c int %i \n", (char) config.finalMConfig, config.finalMConfig);
+            
+            for (int x=0; x < i; x++) {
+                printf("searching config %c next config %c \n", configs[x].finalMConfig, configs[x].mConfig);
+                if (configs[x].mConfig == config.finalMConfig) {
+                    printf("found config %c next config %c \n", configs[x].mConfig, configs[x].finalMConfig);
+                    config = configs[x];
+                    break;
                 }
+            }
+            
         }
         
-        config = configs[atoi(config.finalMConfig)];
         times++;
+        printf("ptrTape %s",tape );
     }
     
-    printf("ptrTape %s",ptrTape );
+   
     printf("done\n");
     
     
